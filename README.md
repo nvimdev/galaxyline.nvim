28afacabafd99032fd5464e30f3483baaea30a4a
Raphael <glepnir@gopherhub.org>
2020-11-01 14:19:45 +0800

    refactor diagnostic

--- README.md
+++ README.md
@@ -85,10 +85,12 @@ local buffer = require('galaxyline.provider_buffer')
 BufferIcon  = buffer.get_buffer_type_icon,
 BufferNumber = buffer.get_buffer_number,
 FileTypeName = buffer.get_buffer_filetype,
+-- Git Provider
 GitBranch = vcs.get_git_branch,
 DiffAdd = vcs.diff_add,             -- support vim-gitgutter vim-signify gitsigns
 DiffModified = vcs.diff_modified,   -- support vim-gitgutter vim-signify gitsigns
 DiffRemove = vcs.diff_remove,       -- support vim-gitgutter vim-signify gitsigns
+-- File Provider
 LineColumn = fileinfo.line_column,
 FileFormat = fileinfo.get_file_format,
 FileEncode = fileinfo.get_file_encode,
@@ -98,8 +100,11 @@ FileName = fileinfo.get_current_file_name,
 LinePercent = fileinfo.current_line_percent,
 ScrollBar = extension.scrollbar_instance,
 VistaPlugin = extension.vista_nearest,
-DiagnosticError = diagnostic.get_diagnostic_error, -- support nvim-lsp coc ale
-DiagnosticWarn = diagnostic.get_diagnostic_warn,   -- support nvim-lsp coc ale
+-- Diagnostic Provider
+DiagnosticError = diagnostic.get_diagnostic_error,
+DiagnosticWarn = diagnostic.get_diagnostic_warn,
+DiagnosticHint = diagnostic.get_diagnostic_hint,
+DiagnosticInfo = diagnostic.get_diagnostic_info,
 
 -- public libs
 require('galaxyline.provider_vcs').get_git_dir() -- find git root
--- lua/galaxyline/provider_diagnostic.lua
+++ lua/galaxyline/provider_diagnostic.lua
@@ -1,123 +1,57 @@
 local vim,lsp = vim,vim.lsp
 local M = {}
 
--- coc error
-local function diagnostic_coc_error()
+-- coc diagnostic
+local function get_coc_diagnostic(diag_type)
   local has_info,info = pcall(vim.fn.nvim_buf_get_var,0,'coc_diagnostic_info')
   if not has_info then return end
-  if info.error > 0 then
-    return info.error
-  end
-  return ''
-end
-
--- coc warning
-local function diagnostic_coc_warn()
-  local has_info,info = pcall(vim.fn.nvim_buf_get_var,0,'coc_diagnostic_info')
-  if not has_info then return end
-  if info.warning > 0 then
-    return  info.warning
-  end
-  return ''
-end
-
--- coc hint
-local function diagnostic_coc_hint()
-  local has_info,info = pcall(vim.fn.nvim_buf_get_var,0,'coc_diagnostic_info')
-  if not has_info then return end
-  if info.hint > 0 then
-    return  info.hint
+  if info[diag_type] > 0 then
+    return  info[diag_type]
   end
   return ''
 end
 
 -- nvim-lspconfig
 -- see https://github.com/neovim/nvim-lspconfig
-local function diagnostic_nvim_lsp_error()
+local function get_nvim_lsp_diagnostic(diag_type)
   if vim.tbl_isempty(lsp.buf_get_clients(0)) then return '' end
-  local count = lsp.util.buf_diagnostics_count('Error')
+  local count = lsp.util.buf_diagnostics_count(diag_type)
   if count ~= 0 then return count end
 end
-
--- nvim-lspconfig
--- see https://github.com/neovim/nvim-lspconfig
-local function diagnostic_nvim_lsp_warn()
-  if vim.tbl_isempty(lsp.buf_get_clients(0)) then return '' end
-  local count = lsp.util.buf_diagnostics_count('Warning')
-  if count ~= 0 then return count end
-end
-
--- nvim-lspconfig
--- see https://github.com/neovim/nvim-lspconfig
-local function diagnostic_nvim_lsp_hint()
-  if vim.tbl_isempty(lsp.buf_get_clients(0)) then return '' end
-  local count = lsp.util.buf_diagnostics_count('Hint')
-  if count ~= 0 then return count end
-end
-
-
--- nvim-lspconfig
--- see https://github.com/neovim/nvim-lspconfig
-local function diagnostic_nvim_lsp_info()
-  if vim.tbl_isempty(lsp.buf_get_clients(0)) then return '' end
-  local count = lsp.util.buf_diagnostics_count('Information')
-  if count ~= 0 then return count end
-end
-
--- ale
--- see https://github.com/dense-analysis/ale
-local function diagnostic_ale_error()
-  local buf_nr = vim.fn.bufnr()
-  local counts = vim.fn['ale#statusline#Count'](buf_nr)
-  local error_counts = counts.error + counts.style_error
-  return error_counts
-end
-
--- ale
--- see https://github.com/dense-analysis/ale
-local function diagnostic_ale_warn()
-  local buf_nr = vim.fn.bufnr()
-  local counts = vim.fn['ale#statusline#Count'](buf_nr)
-  local non_error_counts = counts.total - (counts.error + counts.style_error)
-  return non_error_counts
-end
-
 
 function M.get_diagnostic_error()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
-    return diagnostic_coc_error()
-  elseif vim.fn.exists('*ale#toggle#Enable') == 1 then
-    return diagnostic_ale_error()
+    return get_coc_diagnostic('error')
   elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return diagnostic_nvim_lsp_error()
   end
   return ''
 end
 
 function M.get_diagnostic_warn()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
-    return diagnostic_coc_warn()
-  elseif vim.fn.exists('*ale#toggle#Enable') == 1 then
-    return diagnostic_ale_warn()
+    return get_coc_diagnostic('warning')
   elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return diagnostic_nvim_lsp_warn()
+    return get_nvim_lsp_diagnostic('Warning')
   end
   return ''
 end
 
 function M.get_diagnostic_hint()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
-    return diagnostic_coc_hint()
+    return get_coc_diagnostic('hint')
   elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return diagnostic_nvim_lsp_hint()
+    return get_nvim_lsp_diagnostic('Hint')
   end
   return ''
 end
 
 function M.get_diagnostic_info()
-  if not vim.tbl_isempty(lsp.buf_get_clients(0)) then
-    return diagnostic_nvim_lsp_info()
+  if vim.fn.exists('*coc#rpc#start_server') == 1 then
+    return get_coc_diagnostic('information')
+  elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
+    return get_nvim_lsp_diagnostic('Information')
   end
+  return ''
 end
 
 return M
