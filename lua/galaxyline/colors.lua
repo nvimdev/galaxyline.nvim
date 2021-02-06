90271dafba9c7c3021d29a284e3f1621c06cc40c
bitcoder <glepnir@gopherhub.org>
2021-02-06 15:10:46 +0800

    improve color code

--- lua/galaxyline/colors.lua
+++ lua/galaxyline/colors.lua
@@ -1,3 +1,4 @@
+local api = vim.api
 local M = {}
 
 -- return current background color
@@ -7,40 +8,43 @@ function M.get_background_color()
   return normal_bg
 end
 
+local _switch = {
+  ['string'] = function (hi_type,hi_color)
+    local color = hi_color or 'NONE'
+    return 'gui'..hi_type..'='..color
+  end,
+  ['function'] = function (hi_type,color)
+    local resolved_color = color()
+    if resolved_color == nil or resolved_color == "" then
+      return 'gui'..hi_type..'='..'NONE'
+    end
+    return 'gui'..hi_type..'='..resolved_color
+  end
+}
+
+local _switch_metatable = {
+  __index = function (_,k)
+    print(string.format("expect table or string got %s",type(k)))
+    return
+  end
+}
+
+setmetatable(_switch,_switch_metatable)
+
 local function set_highlight(group, color)
-  local fg,bg,style
+  local fg,bg,style = 'fg','bg',''
 
   if type(color) == 'string' then
-    vim.api.nvim_command('highlight link ' .. group .. ' ' .. color)
+    api.nvim_command('highlight link ' .. group .. ' ' .. color)
     return
   end
 
-  if type(color[1]) == 'function' then
-    local resolved_color = color[1]()
-    if resolved_color ~= nil and resolved_color ~= "" then
-      fg = 'guifg=' .. resolved_color
-    else
-      fg = 'guifg=NONE'
+  if type(color) == 'table' then
+    fg = _switch[type(color[1])](fg,color[1])
+    bg = _switch[type(color[2])](bg,color[2])
+    if color[3] then
+      style = _switch[type(color[3])](style,color[3])
     end
-  else
-    fg = color[1] and 'guifg=' .. color[1] or 'guifg=NONE'
-  end
-
-  if type(color[2]) == 'function' then
-    local resolved_color = color[2]()
-    if resolved_color ~= nil and resolved_color ~= "" then
-      bg = 'guibg=' .. resolved_color
-    else
-      bg = 'guibg=NONE'
-    end
-  else
-    bg = color[2] and 'guibg=' .. color[2] or 'guibg=NONE'
-  end
-
-  if type(color[3]) == 'function' then
-    style = 'gui='.. color[3]()
-  else
-    style = color[3] and 'gui=' .. color[3] or ' '
   end
 
   vim.api.nvim_command('highlight ' .. group .. ' ' .. fg .. ' ' .. bg .. ' '..style)
@@ -51,14 +55,12 @@ local send_section_color = function(section)
     for pos,_ in pairs(section) do
       for _,comps in pairs(section[pos]) do
         for component_name,component_info in pairs(comps) do
-          local highlight = component_info.highlight or {}
-          local separator_highlight = component_info.separator_highlight or {}
-          coroutine.yield('Galaxy' .. component_name,highlight)
-
-          if #separator_highlight ~= 0 then
-            coroutine.yield(component_name..'Separator',separator_highlight)
+          if component_info.highlight then
+            coroutine.yield('Galaxy' .. component_name,component_info.highlight)
           end
-
+          if component_info.separator_highlight then
+            coroutine.yield(component_name..'Separator',component_info.separator_highlight)
+          end
         end
       end
     end
@@ -68,7 +70,7 @@ end
 function M.init_theme(section)
   local producer = send_section_color(section)
   while true do
-    local status,group,highlight = coroutine.resume(producer)
+    local _,group,highlight = coroutine.resume(producer)
     if group and highlight then
       set_highlight(group,highlight)
     end
