2ec4e0eeb880b8610c1255c0903b0187d413b9e7
stephen <glepnir@gopherhub.org>
2021-03-02 16:05:26 +0800

    update

--- lua/galaxyline/provider_lsp.lua
+++ lua/galaxyline/provider_lsp.lua
@@ -1,10 +1,11 @@
 local get_lsp_client = function ()
+  local buf_ft = api.nvim_buf_get_option(0,'filetype')
   local buf_clients = vim.lsp.buf_get_clients()
   if next(buf_clients) == nil then
     return 'No Actice Lsp '
   end
   for _,client in ipairs(buf_clients) do
-    if client.filetypes[vim.bo.filetype] then
+    if client.config.filetypes and client.config.filetypes[buf_ft] then
       return client.name ..' '
     end
   end
