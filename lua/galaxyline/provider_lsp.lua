e48c447a3b6b708bdd58811112272c5792f2b319
stephen <glepnir@gopherhub.org>
2021-03-03 09:56:37 +0800

    fix typo

--- lua/galaxyline/provider_lsp.lua
+++ lua/galaxyline/provider_lsp.lua
@@ -4,7 +4,7 @@ local get_lsp_client = function ()
   local buf_ft = vim.api.nvim_buf_get_option(0,'filetype')
   local buf_clients = vim.lsp.buf_get_clients()
   if next(buf_clients) == nil then
-    return 'No Actice Lsp'
+    return 'No Active Lsp'
   end
   for _,client in ipairs(buf_clients) do
     local filetypes = client.config.filetypes
@@ -12,7 +12,7 @@ local get_lsp_client = function ()
       return client.name
     end
   end
-  return 'No Actice Lsp'
+  return 'No Active Lsp'
 end
 
 return {
