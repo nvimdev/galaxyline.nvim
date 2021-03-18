168fe13d5d825a084366f866cc611ae9a128e0f5
bitcoder <glepnir@gopherhub.org>
2021-03-18 21:42:24 +0800

    fix get clients bug

--- lua/galaxyline/provider_lsp.lua
+++ lua/galaxyline/provider_lsp.lua
@@ -1,11 +1,12 @@
 local get_lsp_client = function (msg)
   msg = msg or 'No Active Lsp'
   local buf_ft = vim.api.nvim_buf_get_option(0,'filetype')
-  local buf_clients = vim.lsp.buf_get_clients()
-  if next(buf_clients) == nil then
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
     return msg
   end
-  for _,client in ipairs(buf_clients) do
+
+  for _,client in ipairs(clients) do
     local filetypes = client.config.filetypes
     if filetypes and vim.fn.index(filetypes,buf_ft) ~= -1 then
       return client.name
